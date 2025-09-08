947df85b0800bcfeeeca09bd11146d06b2e2f277
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-09-08 12:39:47 -0300

    Finalizando estrutura | Adicionando Interatividade React

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
+
+import "./Login.css";
+
 import { FaUser, FaLock } from "react-icons/fa";
 
 const Login = () => {
-    
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    console.log("teste:", username, password);
+
+    console.log("Envio");
+
+  }
+
   return (
 
     <div className="container">
 
-        <form>
+        <form onSubmit={handleSubmit}>
 
             <h1>Acesse o sistema</h1>
 
             <div>
 
-                <input type="email" placeholder="E-mail"/>
+                <input type="email" placeholder="E-mail"
+                onChange={(e) => setUsername(e.target.value)} />
                 <FaUser className="icon" />
 
             </div>
@@ -24,7 +42,29 @@ const Login = () => {
 
             </div>
 
+            <div className="recall-forget">
+
+                <label htmlFor="">
+
+                    <input type="checkbox" />
+                    Lembre de mim
+
+                </label>
+
+                <a href="#">Esqueceu a senha?</a>
+
+            </div>
+
             <button>Entrar</button>
+
+            <div className="signup-link">
+
+                <p>
+                    Não tem uma conta? 
+                    <a href="#">Registrar</a> 
+                </p>
+
+            </div>
 
         </form>
 
